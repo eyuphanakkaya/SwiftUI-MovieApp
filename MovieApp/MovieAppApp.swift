4789506c6e52d47d52c68bf92363b242106fb1e2
Eyuphan Akkaya <akkayaeyupp@gmail.com>
2023-07-28 11:29:42 +0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Eyüphan Akkaya on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
